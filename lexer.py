fd5f4ba120c3ce3820796e2324bf92a9626259ed
Lívia Nascimento <liviapnd@gmail.com>
2021-11-29 16:52:26 -0300

    correção do método reserva

--- lexer.py
+++ lexer.py
@@ -1,9 +1,9 @@
 line = 1
 peek = ' '
-words = [] # dictionary/list substituindo hashtable
+words = {} # dictionary/list substituindo hashtable
 
 def reserva(p):
-    words.append(p.lexeme, p)
+    words['lexeme'] = p
 
 def lexer():
     reserva()
