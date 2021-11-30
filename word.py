20510f48aa87d23dcae827589fc0255d26a31af0
Lívia Nascimento <liviapnd@gmail.com>
2021-11-30 13:39:50 -0300

    correção do import

--- word.py
+++ word.py
@@ -1,6 +1,3 @@
-import sys
-
-sys.path.append("./tag.py")
 import tag
 
 words_reserved = {'and':{'string': '&&', 'tag':tag.AND}, 'eq':{'string':'==', 'tag':tag.EQ},'le':{'string':'<=', 'tag':tag.LE},'minus':{'string':'minus', 'tag':tag.MINUS},'False':{'string':'false', 'tag':tag.FALSE},'or':{'string':'||', 'tag':tag.OR},'ne':{'string':'!=', 'tag':tag.NE},'ge':{'string':'>=', 'tag':tag.GE},'True':{'string':'true', 'tag':tag.TRUE},'temp':{'string':'t', 'tag':tag.TEMP}}
